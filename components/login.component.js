e163cbd1e031f7c31fe2553e9bed5ecae157090e
Dominic Järmann <dominic.jaermann@icloud.com>
2019-10-24 13:27:33 +0200

    showcase

--- components/login.component.js
+++ components/login.component.js
@@ -1,24 +1,26 @@
 import { Greeter } from "../modules/injectme.module";
 
-export class LoginComponent{
-    constructor(state){
-        this.state = state;
-        this.selector = 'login'
-        this.i = 0;
-        this.greeter = this.state.injector.resolve(Greeter)
-    }
-    incrementCounter(){
-      this.i++;
-      return this.i;
-    }
+export class LoginComponent {
+  constructor(state) {
+    this.state = state;
+    this.selector = 'login'
+    this.i = 0;
+    this.greeter = this.state.injector.resolve(Greeter)
+  }
+  incrementCounter() {
+    this.i++;
+    return this.i;
+  }
 
-    render(){
-        return  `
+  render() {
+    /*html*/
+
+    return `
         <div>
           <input type="text" placeholder="Username ">
           <input type="text" placeholder="Username">
           ${this.greeter.greet('Dominic')}
         </div>
       `
-    }
+  }
 }
--- main.js
+++ main.js
@@ -2,29 +2,48 @@ import { LoginComponent } from './components/login.component.js';
 import { Router } from './nails/coreComponents/router.component.js';
 import { Nails } from './nails/nails.js'
 import { Greeter } from './modules/injectme.module.js';
+import { ShowcaseComponent } from './components/showcase.component.js';
+import { NavbarComponent } from './components/navbar.component.js';
 
+
+var guard = function () {
+    return localStorage.getItem('jwt') !== null;
+}
 
 window.nails = new Nails({
     el: "body", //Start with # to specify id
     data: {
         title: "Your Nails App",
         whoami: "NailsJS",
+        sample: [
+            { name: "Jill", lastname: "smith" },
+            { name: "Ingo", lastname: "Meyers" },
+        ]
     },
     methods: {
         onInit() {
             // This method is called during early construction of the State. As a result, there no state supplied as an argument.
-            // You may use this, to trigger your own scripts. Beware, that the dom is not rendert at this time, so use OnMounted for any DOM operations.
+            // You may use this, to trigger your own scripts. Beware, that the dom is not rendered at this time, so use OnMounted for any DOM operations.
         },
         onMounted(currentState) {
             currentState.data.headers = [{ 'Test': 'Value' }];
         },
     },
     components: [
-        LoginComponent, Router
+        LoginComponent, Router, ShowcaseComponent, NavbarComponent
     ],
     routings: [{
         component: LoginComponent,
-        route: 'login'
+        route: 'login',
+    },
+    {
+        component: LoginComponent,
+        route: 'lappe',
+        guard: guard
+    },
+    {
+        component: ShowcaseComponent,
+        route: 'showcase',
     }],
     declarations: [
         Greeter
